cfad790b27ff8e26f65a224a9608796160836206
sijanthapa171 <thapasijan329@gmail.com>
2025-07-07 23:29:31 +0545

    fix: update configuration for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,42 +4,18 @@ export default defineConfig({
   build: {
     minify: 'terser',
     sourcemap: false,
-    lib: {
-      entry: "src/index.ts",
-      name: "materialTheme",
-      fileName: "material-theme",
-      formats: ["umd", "es"],
-    },
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
-      output: {
-        exports: "named",
-        compact: true,
-        minifyInternalExports: true,
-        hoistTransitiveImports: false
+      input: {
+        main: 'index.html',
       },
     },
     terserOptions: {
       compress: {
-        module: true,
         defaults: true,
         drop_console: true,
         drop_debugger: true,
-        dead_code: true,
-        unused: true,
-        passes: 3,         
-        keep_fargs: false, 
-        keep_fnames: false 
-      },
-      format: {
-        comments: false,   
-        beautify: false,   
-        braces: true,      
-        indent_level: 0,   
-        semicolons: false  
-      },
-      mangle: {
-        toplevel: true,    
-        keep_classnames: false
       },
     },
   },
